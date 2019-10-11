efe2ed402f75430a5e5883be5e7e6883327fb847
Aliaksandr Lebiadz <sasha_lebed2001@mail.ru>
2019-10-11 19:09:20 +0000

    Home page data loader is created

---
+++ src/main/java/com/epam/fitness/utils/data/loader/impl/HomePageDataLoader.java
@@ -0,0 +1,11 @@
+package com.epam.fitness.utils.data.loader.impl;
+
+import com.epam.fitness.utils.data.loader.PageDataLoader;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class HomePageDataLoader implements PageDataLoader {
+
+    @Override
+    public void loadDataToSession(HttpServletRequest request) {}
+}
