12ab20a598526168c7280720af3d698f027222dd
Aliaksandr Lebiadz <sasha_lebed2001@mail.ru>
2019-10-12 17:00:42 +0000

    Create ConnectionPoolException

---
+++ src/main/java/com/epam/fitness/exception/ConnectionPoolException.java
@@ -0,0 +1,20 @@
+package com.epam.fitness.exception;
+
+public class ConnectionPoolException extends Exception{
+
+    public ConnectionPoolException() {
+    }
+
+    public ConnectionPoolException(String message) {
+        super(message);
+    }
+
+    public ConnectionPoolException(String message, Exception exception) {
+        super(message, exception);
+    }
+
+    public ConnectionPoolException(Exception exception) {
+        super(exception);
+    }
+
+}
