92900e95f4b7be3d4b544faf586c3bf850f6c989
Aliaksandr Lebiadz <sasha_lebed2001@mail.ru>
2019-10-12 16:35:06 +0000

    Create ExerciseDaoImpl

---
+++ src/main/java/com/epam/fitness/dao/impl/ExerciseDaoImpl.java
@@ -0,0 +1,28 @@
+package com.epam.fitness.dao.impl;
+
+import com.epam.fitness.builder.Builder;
+import com.epam.fitness.dao.api.ExerciseDao;
+import com.epam.fitness.entity.assignment.Exercise;
+
+import java.sql.Connection;
+
+/**
+ * <p>An implementation of the exercise dao interface to provide
+ * an access to the exercise entity in the sql database.</p>
+ */
+public class ExerciseDaoImpl extends AbstractDao<Exercise> implements ExerciseDao {
+
+    private static final String EXERCISE_TABLE = "exercise";
+
+    public ExerciseDaoImpl(Connection connection, Builder<Exercise> builder) {
+        super(connection, builder);
+    }
+
+    @Override
+    public void save(Exercise entity) {}
+
+    @Override
+    protected String getTableName() {
+        return EXERCISE_TABLE;
+    }
+}
