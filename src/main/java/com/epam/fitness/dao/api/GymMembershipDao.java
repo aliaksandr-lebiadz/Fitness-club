ed0070b52e5759d825583fdb83d0684955d44cf1
Aliaksandr Lebiadz <sasha_lebed2001@mail.ru>
2019-10-12 16:29:37 +0000

    Create GymMembershipDao interface

---
+++ src/main/java/com/epam/fitness/dao/api/GymMembershipDao.java
@@ -0,0 +1,9 @@
+package com.epam.fitness.dao.api;
+
+import com.epam.fitness.entity.GymMembership;
+
+/**
+ * <p>An interface specified for a gym membership
+ * entity to provide an access to it.</p>
+ */
+public interface GymMembershipDao extends Dao<GymMembership> {}
