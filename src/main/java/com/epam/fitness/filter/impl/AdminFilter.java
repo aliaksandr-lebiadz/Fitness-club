7ecf70f798d2f96990feea89eb0382489078c0d1
Aliaksandr Lebiadz <sasha_lebed2001@mail.ru>
2019-10-12 17:05:15 +0000

    Create AdminFilter

---
+++ src/main/java/com/epam/fitness/filter/impl/AdminFilter.java
@@ -0,0 +1,27 @@
+package com.epam.fitness.filter.impl;
+
+import com.epam.fitness.entity.user.User;
+import com.epam.fitness.entity.user.UserRole;
+import com.epam.fitness.filter.AbstractFilter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(filterName = "adminFilter", urlPatterns = {"/users"})
+public class AdminFilter extends AbstractFilter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        User user = getUser(request);
+        if(user == null || user.getRole() != UserRole.ADMIN){
+            throw new ServletException("Unauthorized access!");
+        }
+        chain.doFilter(servletRequest, servletResponse);
+    }
+}
