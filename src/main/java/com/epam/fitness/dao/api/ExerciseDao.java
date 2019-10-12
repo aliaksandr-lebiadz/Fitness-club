a9dab7b1c7fec429e605f607957d92183ff6746a
Aliaksandr Lebiadz <sasha_lebed2001@mail.ru>
2019-10-12 16:29:03 +0000

    Create ExerciseDao interface

---
+++ src/main/java/com/epam/fitness/dao/api/ExerciseDao.java
@@ -0,0 +1,9 @@
+package com.epam.fitness.dao.api;
+
+import com.epam.fitness.entity.assignment.Exercise;
+
+/**
+ * <p>An interface specified for an exercise
+ * entity to provide an access to it.</p>
+ */
+public interface ExerciseDao extends Dao<Exercise> {}
