b3ca1c92342077d5fbeca5f6d0fd7b670d348378
Aliaksandr Lebiadz <sasha_lebed2001@mail.ru>
2019-10-13 11:38:27 +0000

    Remove /assignments url

--- src/main/java/com/epam/fitness/filter/impl/ClientFilter.java
+++ src/main/java/com/epam/fitness/filter/impl/ClientFilter.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(filterName = "clientFilter", urlPatterns = {"/getMembership", "/orders", "/assignments"})
+@WebFilter(filterName = "clientFilter", urlPatterns = {"/getMembership", "/orders"})
 public class ClientFilter extends AbstractFilter {
 
     @Override
