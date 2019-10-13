38b26f240d72b28ba07470725f526cd1adcfa6c7
Aliaksandr Lebiadz <sasha_lebed2001@mail.ru>
2019-10-13 10:29:11 +0000

    Add assignments table parameters transfer
    On click on the row of the assignments table, parameters of the 
    selected assignment are being transferred to the
    change-assignment-popup as default values

--- src/main/webapp/scripts/table_select.js
+++ src/main/webapp/scripts/table_select.js
@@ -5,5 +5,11 @@ $(document).ready(function(){
         var $currentRow = $(this).closest('tr');
         var $id = $currentRow.find('td:eq(0)').text();
         $('.hidden-id').val($id);
+        if($(this).parent().parent().attr('id') === 'assignment'){
+            var $amountOfSets = $currentRow.find('td:eq(3)').text();
+            var $amountOfReps = $currentRow.find('td:eq(4)').text();
+            $('#amount-of-sets').val($amountOfSets);
+            $('#amount-of-reps').val($amountOfReps);
+        }
     });
 });
