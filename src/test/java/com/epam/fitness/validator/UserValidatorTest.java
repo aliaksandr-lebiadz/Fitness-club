84fdde503d43067e2aae49124e12a9eec7c8e404
Aliaksandr Lebiadz <sasha_lebed2001@mail.ru>
2019-10-19 18:18:52 +0000

    Create UserValidatorTest

---
+++ src/test/java/com/epam/fitness/validator/UserValidatorTest.java
@@ -0,0 +1,47 @@
+package com.epam.fitness.validator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserValidatorTest {
+
+    private static final int VALID_DISCOUNT = 52;
+    private static final int NEGATIVE_DISCOUNT = -5;
+    private static final int EXCESS_DISCOUNT = 150;
+
+    private UserValidator validator = new UserValidator();
+
+    @Test
+    public void testIsDiscountValidShouldReturnTrueWhenValidDiscountSupplied(){
+        //given
+
+        //when
+        boolean actual = validator.isDiscountValid(VALID_DISCOUNT);
+
+        //then
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void testIsDiscountValidShouldReturnFalseWhenNegativeDiscountSupplied(){
+        //given
+
+        //when
+        boolean actual = validator.isDiscountValid(NEGATIVE_DISCOUNT);
+
+        //then
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void testIsDiscountValidShouldReturnFalseWhenExcessDiscountSupplied(){
+        //given
+
+        //when
+        boolean actual = validator.isDiscountValid(EXCESS_DISCOUNT);
+
+        //then
+        Assert.assertFalse(actual);
+    }
+
+}
