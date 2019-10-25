95f182ac98170be9f2956324e74960a1b02cf0c9
Aliaksandr Lebiadz <sasha_lebed2001@mail.ru>
2019-10-25 21:29:57 +0300

    Remove some methods due to the adding AbstractLocalizer class
    Change the getAttributeName() method's access modifier from private to protected.

--- src/main/java/com/epam/fitness/tag/localizer/AssignmentStatusLocalizerTag.java
+++ src/main/java/com/epam/fitness/tag/localizer/AssignmentStatusLocalizerTag.java
@@ -1,46 +1,17 @@
 package com.epam.fitness.tag.localizer;
 
 import com.epam.fitness.entity.assignment.AssignmentStatus;
-
 import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.JspWriter;
-import javax.servlet.jsp.tagext.BodyTagSupport;
-import java.io.IOException;
 
-public class AssignmentStatusLocalizerTag extends BodyTagSupport {
+public class AssignmentStatusLocalizerTag extends AbstractLocalizer {
 
     private static final String NEW_STATUS = "new_status";
     private static final String ACCEPTED_STATUS = "accepted_status";
     private static final String CHANGED_STATUS = "changed_status";
     private static final String CANCELED_STATUS = "canceled_status";
 
-    private JspWriter writer;
-
     @Override
-    public int doStartTag(){
-        writer = pageContext.getOut();
-        return EVAL_BODY_BUFFERED;
-    }
-
-    @Override
-    public int doAfterBody() throws JspException {
-        String value = bodyContent.getString();
-        String attributeName = getAttributeName(value);
-        try{
-            Object attribute = pageContext.findAttribute(attributeName);
-            writer.print(attribute);
-        } catch (IOException ex){
-            throw new JspException(ex.getMessage(), ex);
-        }
-        return SKIP_BODY;
-    }
-
-    @Override
-    public int doEndTag() {
-        return EVAL_PAGE;
-    }
-
-    private String getAttributeName(String statusValue) throws JspException{
+    protected String getAttributeName(String statusValue) throws JspException{
         AssignmentStatus status = AssignmentStatus.valueOf(statusValue);
         String attributeName;
         switch (status){
