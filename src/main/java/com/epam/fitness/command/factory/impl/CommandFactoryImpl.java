bfcc0ac04d6ec4b43fae3f0497ff66558650014d
Aliaksandr Lebiadz <sasha_lebed2001@mail.ru>
2019-10-28 00:50:13 +0300

    Change the some commands creation

--- src/main/java/com/epam/fitness/command/factory/impl/CommandFactoryImpl.java
+++ src/main/java/com/epam/fitness/command/factory/impl/CommandFactoryImpl.java
@@ -2,7 +2,6 @@ package com.epam.fitness.command.factory.impl;
 
 import com.epam.fitness.command.Command;
 import com.epam.fitness.command.factory.CommandFactory;
-import com.epam.fitness.command.factory.CommandFactoryHelper;
 import com.epam.fitness.command.impl.*;
 import com.epam.fitness.command.impl.assignment.ChangeAssignmentCommand;
 import com.epam.fitness.command.impl.order.SendFeedbackCommand;
@@ -24,9 +23,6 @@ import com.epam.fitness.validator.impl.UserValidatorImpl;
 
 public class CommandFactoryImpl implements CommandFactory {
 
-    private static final String HOME_PAGE_URL = "/home";
-    private static final String USERS_PAGE_URL = "/users";
-    private static final String ORDER_PAGE_URL = "/getMembership";
     private static final String LOGIN_COMMAND = "login";
     private static final String LOG_OUT_COMMAND = "logOut";
     private static final String SHOW_HOME_PAGE_COMMAND = "showHomePage";
@@ -37,20 +33,17 @@ public class CommandFactoryImpl implements CommandFactory {
     public static final String SHOW_ORDERS_COMMAND = "showOrders";
     public static final String SEND_FEEDBACK_COMMAND = "sendFeedback";
     public static final String SHOW_TRAINER_CLIENTS_COMMAND = "showTrainerClients";
-    public static final String SHOW_TRAINER_CLIENT_ORDERS_COMMAND = "showTrainerClientOrders";
     public static final String ASSIGN_NUTRITION_TYPE_COMMAND = "assignNutritionType";
     public static final String SHOW_ASSIGNMENTS_COMMAND = "showAssignments";
     public static final String CHANGE_ASSIGNMENT_STATUS_COMMAND = "changeAssignmentStatus";
     public static final String CHANGE_ASSIGNMENT_COMMAND = "changeAssignment";
-    public static final String SHOW_USERS_PAGE_COMMAND = "showUsersPage";
+    public static final String SHOW_CLIENTS_COMMAND = "showClients";
     public static final String SET_USER_DISCOUNT_COMMAND = "setUserDiscount";
 
     private DaoFactory factory;
-    private CommandFactoryHelper helper;
 
-    public CommandFactoryImpl(DaoFactory factory, CommandFactoryHelper helper){
+    public CommandFactoryImpl(DaoFactory factory){
         this.factory = factory;
-        this.helper = helper;
     }
 
 
@@ -63,9 +56,8 @@ public class CommandFactoryImpl implements CommandFactory {
                 );
                 break;
             case SHOW_ORDER_PAGE_COMMAND:
-                command = new ShowPageCommand(
-                        ORDER_PAGE_URL,
-                        helper.getPageDataLoader(ORDER_PAGE_URL)
+                command = new ShowOrderPageCommand(
+                        new GymMembershipServiceImpl(factory)
                 );
                 break;
             case GET_MEMBERSHIP_COMMAND:
@@ -88,12 +80,8 @@ public class CommandFactoryImpl implements CommandFactory {
             case SHOW_TRAINER_CLIENTS_COMMAND:
                 command = new ShowTrainerClientsCommand(
                         new UserServiceImpl(factory),
+                        getOrderService(),
                         new ExerciseServiceImpl(factory)
-                );
-                break;
-            case SHOW_TRAINER_CLIENT_ORDERS_COMMAND:
-                command = new ShowTrainerClientOrdersCommand(
-                        getOrderService()
                 );
                 break;
             case ASSIGN_NUTRITION_TYPE_COMMAND:
@@ -118,16 +106,9 @@ public class CommandFactoryImpl implements CommandFactory {
                         new AssignmentValidatorImpl()
                 );
                 break;
-            case SHOW_USERS_PAGE_COMMAND:
-                command = new ShowPageCommand(
-                        USERS_PAGE_URL,
-                        helper.getPageDataLoader(USERS_PAGE_URL)
-                );
-                break;
-            case SHOW_HOME_PAGE_COMMAND:
-                command = new ShowPageCommand(
-                        HOME_PAGE_URL,
-                        helper.getPageDataLoader(HOME_PAGE_URL)
+            case SHOW_CLIENTS_COMMAND:
+                command = new ShowClientsCommand(
+                        new UserServiceImpl(factory)
                 );
                 break;
             case SET_USER_DISCOUNT_COMMAND:
@@ -135,6 +116,9 @@ public class CommandFactoryImpl implements CommandFactory {
                         new UserServiceImpl(factory),
                         new UserValidatorImpl()
                 );
+                break;
+            case SHOW_HOME_PAGE_COMMAND:
+                command = new ShowHomePageCommand();
                 break;
             case LOG_OUT_COMMAND:
                 command =  new LogOutCommand();
