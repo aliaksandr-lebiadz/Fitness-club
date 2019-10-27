e2d6ff34f3b9d04c76c52da2d2f01757efa3b2f7
Aliaksandr Lebiadz <sasha_lebed2001@mail.ru>
2019-10-28 00:38:08 +0300

    Rename some commands, delete ShowTrainerClientOrdersCommand

--- src/main/java/com/epam/fitness/filter/helper/CommandAccessController.java
+++ src/main/java/com/epam/fitness/filter/helper/CommandAccessController.java
@@ -14,13 +14,12 @@ public class CommandAccessController {
 
         UserRole role = user.getRole();
         switch (command){
-            case SHOW_USERS_PAGE_COMMAND:
+            case SHOW_CLIENTS_COMMAND:
             case SET_USER_DISCOUNT_COMMAND:
                 return role == UserRole.ADMIN;
 
             case ASSIGN_NUTRITION_TYPE_COMMAND:
             case SHOW_TRAINER_CLIENTS_COMMAND:
-            case SHOW_TRAINER_CLIENT_ORDERS_COMMAND:
                 return role == UserRole.TRAINER;
 
             case GET_MEMBERSHIP_COMMAND:
