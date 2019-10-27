95998c81a61a67203ff6c68ab7644ed6ff98f7d1
Aliaksandr Lebiadz <sasha_lebed2001@mail.ru>
2019-10-28 00:34:23 +0300

    Remove unnecessary classes for loading data to the session

--- src/main/java/com/epam/fitness/command/factory/CommandFactoryHelper.java
+++
@@ -1,9 +0,0 @@
-package com.epam.fitness.command.factory;
-
-import com.epam.fitness.utils.data.loader.PageDataLoader;
-
-public interface CommandFactoryHelper {
-
-    PageDataLoader getPageDataLoader(String pageUrl);
-
-}
