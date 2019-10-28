6d2497360a60049806c8af6cbc92ed8f38501d6f
Aliaksandr Lebiadz <sasha_lebed2001@mail.ru>
2019-10-28 20:25:37 +0300

    Change the getPriceWithDiscount() method's name to calculatePriceWithDiscount()

--- src/test/java/com/epam/fitness/utils/OrderUtilsTest.java
+++ src/test/java/com/epam/fitness/utils/OrderUtilsTest.java
@@ -1,14 +1,8 @@
 package com.epam.fitness.utils;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-
 import java.math.BigDecimal;
-import java.util.Calendar;
-import java.util.Date;
-
-import static org.mockito.Mockito.mock;
 
 public class OrderUtilsTest {
 
@@ -17,42 +11,42 @@ public class OrderUtilsTest {
     private OrderUtils utils = new OrderUtils();
 
     @Test
-    public void testGetPriceWithDiscountShouldReturnSamePriceWhenZeroSupplied(){
+    public void testCalculatePriceWithDiscountShouldReturnSamePriceWhenZeroSupplied(){
         //given
         final int discount = 0;
         final BigDecimal initialPrice = new BigDecimal(2.55);
         final BigDecimal expected = new BigDecimal(2.55);
 
         //when
-        BigDecimal actual = utils.getPriceWithDiscount(initialPrice, discount);
+        BigDecimal actual = utils.calculatePriceWithDiscount(initialPrice, discount);
 
         //then
         Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void testGetPriceWithDiscountShouldReturnZeroWhenOneHundredPercentDiscountSupplied(){
+    public void testCalculatePriceWithDiscountShouldReturnZeroWhenOneHundredPercentDiscountSupplied(){
         //given
         final int discount = 100;
         final BigDecimal initialPrice = new BigDecimal(52.1);
         final double expected = 0.0;
 
         //when
-        BigDecimal actual = utils.getPriceWithDiscount(initialPrice, discount);
+        BigDecimal actual = utils.calculatePriceWithDiscount(initialPrice, discount);
 
         //then
         Assert.assertEquals(expected, actual.doubleValue(), DOUBLE_DELTA);
     }
 
     @Test
-    public void testGetPriceWithDiscountShouldReturnHalfOfPriceWhenFiftyPercentDiscountSupplied(){
+    public void testCalculatePriceWithDiscountShouldReturnHalfOfPriceWhenFiftyPercentDiscountSupplied(){
         //given
         final int discount = 50;
         final BigDecimal initialPrice = new BigDecimal(11.77);
         final double expected = 5.885;
 
         //when
-        BigDecimal actual = utils.getPriceWithDiscount(initialPrice, discount);
+        BigDecimal actual = utils.calculatePriceWithDiscount(initialPrice, discount);
 
         //then
         Assert.assertEquals(expected, actual.doubleValue(), DOUBLE_DELTA);
